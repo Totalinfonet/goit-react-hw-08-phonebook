1fc36abafa07233ce234eb4025b00d1a1e6d30ac
Totalinfonet <totalinfonet@gmail.com>
2023-05-06 12:58:33 +0300

    RefreshUser Added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,5 @@
-import React from 'react';
+import { useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
-
 import ContactsPage from 'pages/ContactsPage';
 import Register from 'pages/Register';
 import Login from 'pages/Login';
@@ -8,9 +7,20 @@ import HomePage from 'pages/HomePage';
 import { Layout } from 'components/Layout/Layout';
 import { PrivateRoute } from 'components/PrivateRoute';
 import { RestrictedRoute } from 'components/RestictedRoute';
-
+import { useDispatch } from 'react-redux';
+import { useAuth } from 'hooks/useAuth';
+import { refreshUser } from 'redux/auth/operations';
 export const App = () => {
-  return (
+  const dispatch = useDispatch();
+  const { isRefreshing } = useAuth();
+
+  useEffect(() => {
+    dispatch(refreshUser());
+  }, [dispatch]);
+
+  return isRefreshing ? (
+    <b>Refreshing user...</b>
+  ) : (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -8,7 +8,7 @@ export const UserMenu = () => {
 
   return (
     <div>
-      <p>Welcome, {user.name} </p>
+      <p>Welcome, {user.email} </p>
       <button type="button" onClick={() => dispatch(logOut())}>
         LogOut
       </button>
