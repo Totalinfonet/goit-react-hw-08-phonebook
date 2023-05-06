3daf55c2317bb1dbe3375b92a8902c1e8fb85fdd
Totalinfonet <totalinfonet@gmail.com>
2023-05-06 10:39:40 +0300

    IsLoggedIn Render Added

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,13 +1,15 @@
 import { AuthNav } from 'components/AuthNav/AuthNav';
 import { Navigation } from 'components/Navigation/Navigation';
 import { UserMenu } from 'components/UserMenu/UserMenu';
+import { useAuth } from '../../hooks/useAuth';
 
 export const AppBar = () => {
+  const { isLoggedIn } = useAuth();
+
   return (
     <header>
       <Navigation />
-      <UserMenu />
-      <AuthNav />
+      {isLoggedIn ? <UserMenu /> : <AuthNav />}
     </header>
   );
 };
