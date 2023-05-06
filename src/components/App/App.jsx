597f9d9f5a6215b20bb3ce6400de1020da3ac655
Totalinfonet <totalinfonet@gmail.com>
2023-05-06 10:57:56 +0300

    PrivareRoute Added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -6,13 +6,19 @@ import Register from 'pages/Register';
 import Login from 'pages/Login';
 import HomePage from 'pages/HomePage';
 import { Layout } from 'components/Layout/Layout';
+import { PrivateRoute } from 'components/PrivateRoute';
 
 export const App = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
-        <Route path="/contacts" element={<ContactsPage />} />
+        <Route
+          path="/contacts"
+          element={
+            <PrivateRoute redirectTo="/login" component={<ContactsPage />} />
+          }
+        />
         <Route path="/register" element={<Register />} />
         <Route path="/login" element={<Login />} />
       </Route>
