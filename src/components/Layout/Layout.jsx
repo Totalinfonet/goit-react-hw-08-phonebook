1e77f3a2316e5cd5fa0a7e169452577818b43f69
Totalinfonet <totalinfonet@gmail.com>
2023-05-06 16:52:37 +0300

    Layout & Home Styles Added

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,11 +1,16 @@
+import { Box } from '@chakra-ui/react';
 import { AppBar } from 'components/AppBar/AppBar';
 import { Outlet } from 'react-router-dom';
 
 export const Layout = () => {
   return (
-    <div>
+    <Box
+      bgGradient="linear(to-r, gray.300, yellow.400, pink.200)"
+      minH="100vh"
+      px="4"
+    >
       <AppBar />
       <Outlet />
-    </div>
+    </Box>
   );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,29 @@
+import { Box, Heading, Text } from '@chakra-ui/react';
+
 const HomePage = () => {
   return (
-    <div>
-      <h1>Welcome Bro</h1>
-    </div>
+    <Box maxW="600px" mx="auto" my="8">
+      <Heading
+        as="h1"
+        size="xl"
+        mb="4"
+        bgGradient="linear(to-l, #7928CA, #FF0080)"
+        bgClip="text"
+        fontSize="6xl"
+        fontWeight="extrabold"
+      >
+        Welcome to our phonebook app!
+      </Heading>
+      <Text fontSize="lg" lineHeight="tall">
+        Our phonebook app is designed to make it easy for you to store and
+        access all of your important contacts in one convenient location. With
+        our user-friendly interface and robust search functionality, you'll
+        never have to worry about losing a contact or struggling to find the
+        information you need. So why wait? Sign up and start enjoying the
+        benefits of streamlined contact management. We can't wait to help you
+        stay connected!
+      </Text>
+    </Box>
   );
 };
 
