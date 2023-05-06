a1bd3a25bbe6c2adbdbf48fcc0de8816237bbd25
Totalinfonet <totalinfonet@gmail.com>
2023-05-06 14:05:09 +0300

    Chakra UI Lib Added

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,15 +1,35 @@
+// import { AuthNav } from 'components/AuthNav/AuthNav';
+// import { Navigation } from 'components/Navigation/Navigation';
+// import { UserMenu } from 'components/UserMenu/UserMenu';
+// import { useAuth } from '../../hooks/useAuth';
+
+// export const AppBar = () => {
+//   const { isLoggedIn } = useAuth();
+
+//   return (
+//     <header>
+//       <Navigation />
+//       {isLoggedIn ? <UserMenu /> : <AuthNav />}
+//     </header>
+//   );
+// };
+
 import { AuthNav } from 'components/AuthNav/AuthNav';
 import { Navigation } from 'components/Navigation/Navigation';
 import { UserMenu } from 'components/UserMenu/UserMenu';
 import { useAuth } from '../../hooks/useAuth';
+import { Box, Flex, Spacer } from '@chakra-ui/react';
 
 export const AppBar = () => {
   const { isLoggedIn } = useAuth();
 
   return (
-    <header>
-      <Navigation />
-      {isLoggedIn ? <UserMenu /> : <AuthNav />}
-    </header>
+    <Box bg="gray.600" color="white" boxShadow="2xl" py={4} px={8}>
+      <Flex alignItems="center">
+        <Navigation />
+        <Spacer />
+        {isLoggedIn ? <UserMenu /> : <AuthNav />}
+      </Flex>
+    </Box>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,15 +3,18 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from './redux/store';
 import { Provider } from 'react-redux';
+import { ChakraProvider } from '@chakra-ui/react';
 import { App } from './components/App/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/goit-react-hw-08-phonebook">
-        <App />
-      </BrowserRouter>
+      <ChakraProvider>
+        <BrowserRouter basename="/goit-react-hw-08-phonebook">
+          <App />
+        </BrowserRouter>
+      </ChakraProvider>
     </Provider>
   </React.StrictMode>
 );
